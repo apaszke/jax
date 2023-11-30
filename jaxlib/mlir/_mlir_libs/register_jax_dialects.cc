cb6248cf1b493c39708c509a14cc22db040cfa6b
Adam Paszke <adam.paszke@gmail.com>
2023-11-30 14:21:55 +0000

    Expose the gpu, nvgpu and nvvm dialects in jaxlib

--- jaxlib/mlir/_mlir_libs/register_jax_dialects.cc
+++ jaxlib/mlir/_mlir_libs/register_jax_dialects.cc
@@ -7,7 +7,11 @@
 #include "mlir-c/Dialect/MemRef.h"
 #include "mlir-c/Dialect/SCF.h"
 #include "mlir-c/Dialect/Vector.h"
+#include "mlir-c/Dialect/GPU.h"
+#include "mlir-c/Dialect/NVGPU.h"
+#include "mlir-c/Dialect/NVVM.h"
 #include "mlir-c/Transforms.h"
+#include "mlir-c/Conversion.h"
 #include "mlir/Bindings/Python/PybindAdaptors.h"
 
 #define REGISTER_DIALECT(name) \
@@ -24,6 +28,12 @@ PYBIND11_MODULE(register_jax_dialects, m) {
     REGISTER_DIALECT(memref);
     REGISTER_DIALECT(scf);
     REGISTER_DIALECT(vector);
+    REGISTER_DIALECT(gpu);
+    REGISTER_DIALECT(nvgpu);
+    REGISTER_DIALECT(nvvm);
+    mlirRegisterGPUPasses();
+    mlirRegisterMemRefPasses();
+    mlirRegisterConversionPasses();
     mlirRegisterTransformsPasses();
     // Transforms used by JAX.
     mlirRegisterTransformsStripDebugInfo();
